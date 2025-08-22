e0816c5d432c4cf68b7e45576b7f89b21b3413b1
M Deniz Cengiz <61910060+mdenizcengiz@users.noreply.github.com>
2025-08-22 12:44:30 -0500

    Create WN10-CC-000005.ps1

---
+++ STIGS/WN10-CC-000005.ps1
@@ -0,0 +1,32 @@
+.SYNOPSIS
+    Camera access from the lock screen must be disabled.
+.NOTES
+    Author          : M. Deniz Cengiz
+    LinkedIn        : linkedin.com/in/mdenizcengiz/
+    GitHub          : github.com/mdenizcengiz
+    Date Created    : 2025-08-22
+    Last Modified   : 2025-08-22
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000005
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Enter below command into powershell and execute it.
+   
+    PS C:\> .\WN10-CC-000005.ps1 
+
+#CODE 
+
+# WN10-CC-000005: Disable camera on the lock screen
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
+New-Item -Path $regPath -Force | Out-Null
+Set-ItemProperty -Path $regPath -Name "NoLockScreenCamera" -Type DWord -Value 1
+gpupdate /target:computer /force | Out-Null
+Write-Output "Set NoLockScreenCamera=1 (camera disabled on the lock screen)."
